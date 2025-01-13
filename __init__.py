bbda583f7e43934488dbcccbc3cba8cdefcc259c
glendenpeh10 <glendenbeast@gmail.com>
2025-01-13 12:03:08 +0800

    added 2 files

--- __init__.py
+++ __init__.py
@@ -270,14 +270,6 @@ def delete_rewards(id):
     return redirect(url_for('rewards_index'))
 
 
-rewards = [
-    {"id": 1, "name": "$5 voucher", "points_required": 500, "description": None},
-    {"id": 2, "name": "$15 voucher", "points_required": 1500, "description": None},
-    {"id": 3, "name": "20% voucher", "points_required": 2000, "description": "$60 minimum spend"},
-    {"id": 4, "name": "$20 voucher", "points_required": 2000, "description": None},
-    {"id": 5, "name": "$2 voucher", "points_required": 200, "description": None},
-    {"id": 6, "name": "Free shipping voucher", "points_required": 1350, "description": None}
-]
 
 user_points = 8888
 
@@ -287,16 +279,23 @@ def rewards_page():
     global user_points
     message = None
     if request.method == 'POST':
-        reward_id = int(request.form.get('reward_id'))
-        reward = next((r for r in rewards if r['id'] == reward_id), None)
-        if reward:
-            if user_points >= reward['points_required']:
-                user_points -= reward['points_required']
-                message = f"Successfully redeemed {reward['name']}!"
-            else:
-                message = "Not enough points to redeem this reward."
+        reward_id = request.form.get('reward_id')
+        reward = Reward.query.get(reward_id)
 
-    return render_template('rewards.html', rewards=rewards, user_points=user_points, message=message)
+        if reward and user_points >= reward.points_required:
+            user_points -= reward.points_required
+            message = f"Successfully redeemed {reward.name}!"
+        else:
+            message = "You don't have enough points to redeem this reward."
+
+    rewards = Reward.query.all()
+
+    return render_template(
+        'rewards.html',
+        rewards=rewards,
+        user_points=user_points,
+        message=message
+    )
 
 
 @app.route("/inventory", methods=["GET"])
