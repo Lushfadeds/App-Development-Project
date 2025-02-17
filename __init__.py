a0d1bde1f0915ef2107e320d71c5cc81d0339fcd
wesley <wesleysinhengyoung@gmail.com>
2025-02-17 12:12:29 +0800

    yes

--- __init__.py
+++ __init__.py
@@ -22,7 +22,7 @@ UPLOAD_FOLDER = 'static'
 app.config['UPLOAD_FOLDER'] = 'static'
 inventory_manager = Inventory()
 items = []
-print("hi")
+print("yo")
 
 Allowed_Extensions = {'png', 'jpg', 'jpeg'}
 
